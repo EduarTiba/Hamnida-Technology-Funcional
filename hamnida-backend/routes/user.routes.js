15296f561206d0c24aeff65512a029f88f9d647e
EduarTiba <159581188+EduarTiba@users.noreply.github.com>
2025-05-07 15:01:45 -0500

    Update user.routes.js

--- hamnida-backend/routes/user.routes.js
+++ hamnida-backend/routes/user.routes.js
@@ -3,9 +3,14 @@ const router = express.Router();
 const { registrarUsuario, loginUsuario, deleteUser } = require('../controllers/user.controller'); // Asegúrate de importar todos los controladores
 const { verifyToken, isAdmin } = require('../middleware/auth.middleware');
 
+router.get('/admin-only', verifyToken, isAdmin, (req, res) => {
+    res.json({ msg: 'Acceso de administrador exitoso' });
+  });
 // Rutas de usuarios
 router.post('/register', registrarUsuario); // POST /api/users/register
 router.post('/login', loginUsuario); // POST /api/users/login ✅
-router.delete('/:id', verifyToken, isAdmin, deleteUser); // DELETE /api/users/:id ✅
+router.delete('/:id', verifyToken, isAdmin, deleteUser) => {
+    res.json({ msg: 'Acceso de administrador exitoso' });
+  }); // DELETE /api/users/:id ✅
 
 module.exports = router;

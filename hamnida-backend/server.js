d3435b42f57892dfc64673ec1cc18b089f819022
EduarTiba <159581188+EduarTiba@users.noreply.github.com>
2025-05-07 15:17:41 -0500

    Update server.js

--- hamnida-backend/server.js
+++ hamnida-backend/server.js
@@ -1,7 +1,6 @@
-// server.js
 const app = require('./app');
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
